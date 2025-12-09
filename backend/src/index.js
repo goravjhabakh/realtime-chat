7d2aee68311fb686958d08d78e8fc983014c06e1
goravjhabakh <goravjhabakh@gmail.com>
2025-12-09 15:28:52 +0530

    Deployment ready - bug fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,8 +17,11 @@ app.use('/api/message', messageRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+  console.log('path1:', path.join(__dirname, '../frontend/dist'))
+
+  app.get(/.*/, (req, res) => {
+    console.log('path2:', path.join(__dirname, '../frontend/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
   })
 }
 
